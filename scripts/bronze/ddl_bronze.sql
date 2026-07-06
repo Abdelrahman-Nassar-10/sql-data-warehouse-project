9919367a158e2f646353a9ad5fb9bf313a15905d
Abdelrahman Nassar <abdonassar027@gmail.com>
2026-07-06 12:23:34 +0300

    Create bronze schema tables for customer and product data

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,73 @@
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+GO
+CREATE TABLE bronze.crm_cust_info(
+	cst_id INT,
+	cst_key VARCHAR(50),
+	cst_firstname VARCHAR(50),
+	cst_lastname VARCHAR(50),
+	cst_marital_status VARCHAR(50),
+	cst_gndr VARCHAR(50),
+	cst_create_date DATE
+)
+GO
+
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+GO
+CREATE TABLE bronze.crm_prd_info(
+	prd_id INT,
+	prd_key VARCHAR(50),
+	prd_nm VARCHAR(100),
+	prd_cost DECIMAL(10,2),
+	ord_line VARCHAR(50),
+	prd_start_dt DATETIME,
+	ord_end_dt DATETIME
+)
+GO
+
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
+GO
+CREATE TABLE bronze.crm_sales_details(
+	sls_ord_num VARCHAR(50),
+	sls_prd_key VARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales DECIMAL(10,2),
+	sls_quantity INT,
+	sls_price DECIMAL(10,2)
+)
+GO
+
+IF OBJECT_ID('bronze.erp_CUST_AZ12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_CUST_AZ12;
+GO
+CREATE TABLE bronze.erp_CUST_AZ12(
+	CID VARCHAR(50),
+	BDATE DATE,
+	GEN VARCHAR(20)
+)
+GO
+
+IF OBJECT_ID('bronze.erp_LOC_A101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_LOC_A101;
+GO
+CREATE TABLE bronze.erp_LOC_A101(
+	CID VARCHAR(50),
+	CNTRY VARCHAR(50)
+)
+GO
+
+IF OBJECT_ID('bronze.erp_PX_CAT_G1V2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_PX_CAT_G1V2;
+GO
+CREATE TABLE bronze.erp_PX_CAT_G1V2(
+	ID VARCHAR(50),
+	CAT VARCHAR(50),
+	SUBCAT VARCHAR(50),
+	MAINTENANCE VARCHAR(10)
+)
+
